cc38947b09a895462c6f5b34e3d0085310b6c270
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-09-09 10:31:13 -0400

    Updated package to try and fix release issue

--- package.py
+++ package.py
@@ -2,22 +2,17 @@
 
 name = 'ninja'
 
-version = '1.10.1'
+version = '1.10.1+ta.1.0.0'
 
 authors = [
     'philip.luk',
     'benjamin.skinner',
 ]
 
-requires = [
-]
-
-build_requires = [
-]
+build_system = "cmake"
 
 variants = [
-    ['platform-windows', 'visual_studio-2017.15.9'],
-    ['platform-linux'],
+    ['platform-windows', 'arch-x64', 'os-windows-10', 'visual_studio-2017.15.9'],
 ]
 
 def commands():
