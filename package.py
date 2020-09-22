f23263f38e8bff035ba847ce1e3e5147af90f2aa
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-09-22 09:52:04 -0400

    Updated vs to be private build time dep, not runtime

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'ninja'
 
-version = '1.10.1+ta.1.0.0'
+version = '1.10.1+ta.1.0.1'
 
 authors = [
     'philip.luk',
@@ -11,8 +11,16 @@ authors = [
 
 build_system = "cmake"
 
+@early()
+def private_build_requires():
+    import sys
+    if 'win' in str(sys.platform):
+        return ['visual_studio']
+    else:
+        return ['gcc-7']
+
 variants = [
-    ['platform-windows', 'arch-x64', 'os-windows-10', 'visual_studio-2017.15.9'],
+    ['platform-windows', 'arch-x64', 'os-windows-10'],
 ]
 
 def commands():
